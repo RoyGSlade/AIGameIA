3a1eaa9b71dddf6d0e50ffd13879ce77af1c38c2
RoyGSlade <110950006+RoyGSlade@users.noreply.github.com>
2025-06-04 14:40:36 -0700

    fix: update paths for public index

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/Game', express.static('Game'));
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/public/index.html');
