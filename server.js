a70201b3d2363b9592dde635cc49c67fcc264157
RoyGSlade <donavencrenshaw@gmail.com>
2025-06-04 14:37:11 -0700

    Added node.js

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import fetch from 'node-fetch';
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/public/index.html');
+});
+
+// Example endpoint
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
